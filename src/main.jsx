0257f64eed32bfb70b4eef45127275ed2adab833
Woolf <woolfson1995@gmail.com>
2025-02-26 20:16:08 +0200

    create hw

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './components/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+import "modern-normalize";
+import "./index.css";
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import './index.css'
-import App from './components/App'
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
